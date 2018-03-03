3fb2d38fd76163010caef033da36111d5f51d2b7
Igor Zalutski <izalutski@gmail.com>
2018-03-03 20:34:33 +0000

    Add ES population script

--- populateElastic.js
+++ populateElastic.js
@@ -3,18 +3,34 @@
 /* Populates ES cluster with items from json file */
 
 const elasticsearch = require('elasticsearch');
+const data = require('./fatlama.json');
+const _ = require('lodash');
 
 const client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'trace'
+    host: 'localhost:9200',
+    log: 'trace'
 });
 
-client.ping({
-    requestTimeout: 1000
-}, (error) => {
+const bulk = _.flatten(data.items.map(item => {
+    return [
+        { index: { _index: 'items', _type: 'fl-item' } },
+        {
+            'item_name': item.item_name,
+            'item_url': item.item_url,
+            location: {
+                lat: item.lat,
+                lon: item.lng,
+            }
+        }
+    ];
+}));
+
+client.bulk({
+    body: bulk
+}, (error, response) => {
     if (error) {
-        console.trace('elasticsearch cluster is down!');
+        console.trace(error);
     } else {
-        console.log('All is well');
+        console.log(response);
     }
 });
