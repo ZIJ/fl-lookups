63bc30727f09c435dee1fa31655421969a2a2fb7
Igor Zalutski <izalutski@gmail.com>
2018-03-05 04:57:45 +0000

    Decouple repository from es client; update structure

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const express = require('express');
+const elasticsearch = require('elasticsearch');
 const config = require('./config');
-const EsItemsRepository = require('./esItemsRepository');
-const itemsRouter = require('./itemsRouter');
+const EsItemsRepository = require('./repositories/esItemsRepository');
+const itemsRouter = require('./routers/itemsRouter');
 
 const app = express();
-const itemsRepository = new EsItemsRepository(config.elasticsearch);
-app.use('/api/v1/items', itemsRouter(itemsRepository));
+const esClient = new elasticsearch.Client(config.elasticsearch)
+const itemsRepository = new EsItemsRepository(esClient);
 
+app.use('/api/v1/items', itemsRouter(itemsRepository));
 app.listen(3000, () => console.log('Listening on port 3000'));
--- routers/itemsRouter.js
+++ routers/itemsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const simpleSearch = require('./service/items/simpleSearch');
-const some = require('./service/items/some');
+const simpleSearch = require('../service/items/simpleSearch');
+const some = require('../service/items/some');
 
 module.exports = (itemsRepository) => {
     const itemsRouter = express.Router();
